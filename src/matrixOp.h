46d8e6149a5f6d88043fac028a473c400598e6e7
Max Weber <maxweber1603@gmail.com>
2025-12-10 16:32:40 +0800

    feat: adds header file for matrix operations

---
+++ src/matrixOp.h
@@ -0,0 +1,12 @@
+#ifndef MATRIXOP_H
+#define MATRIXOP_H
+
+int  elemMult(int[][]);
+int mult(int[][]);
+int transpose(int[][]);
+int det(int[][]);
+int adjoint(int[][]);
+int inverse(int[][]);
+
+
+#endif
