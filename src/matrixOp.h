5f9e9ddc75d23397a0a031103df1d91699520b72
Max Weber <maxweber1603@gmail.com>
2025-12-10 19:44:21 +0800

    feat: Adds working library for each matrix operation with main file to test every function using user input for matrices

--- src/matrixOp.h
+++ src/matrixOp.h
@@ -1,12 +1,14 @@
 #ifndef MATRIXOP_H
 #define MATRIXOP_H
 
-int  elemMult(int a[][], int b[][]);
-int mult(int a[][], int b[][]);
-int transpose(int a[][]);
-int det(int a[][]);
-int adjoint(int a[][]);
-int inverse(int a[][]);
+typedef int Matrix3x3[3][3];
 
+void elemMult(Matrix3x3 a, Matrix3x3 b, Matrix3x3 result);
+void mult(Matrix3x3 a, Matrix3x3 b, Matrix3x3 result);
+void transpose(Matrix3x3 a, Matrix3x3 result);
+float  det3x3(Matrix3x3 a);
+float  det2x2(int m[2][2]);
+void adjoint(Matrix3x3 a, Matrix3x3 result);
+void inverse(Matrix3x3 a,  float result[3][3]);
 
 #endif
